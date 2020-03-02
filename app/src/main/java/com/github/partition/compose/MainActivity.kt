513b2c2ac84408f1f98d299248c0af7480b9881c
Mateusz Herych <heniekk@gmail.com>
2020-03-02 00:54:09 +0000

    Imports

--- app/src/main/java/com/github/partition/compose/MainActivity.kt
+++ app/src/main/java/com/github/partition/compose/MainActivity.kt
@@ -2,14 +2,8 @@ package com.github.partition.compose
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.Composable
 import androidx.lifecycle.lifecycleScope
-import androidx.ui.core.Text
-import androidx.ui.core.TextField
 import androidx.ui.core.setContent
-import androidx.ui.layout.Column
-import androidx.ui.material.Button
-import androidx.ui.material.CircularProgressIndicator
 import androidx.ui.material.MaterialTheme
 import org.koin.android.viewmodel.ext.android.viewModel
 
